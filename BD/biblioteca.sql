bf69299d389b41e9044f93c34068d282301b4040
Aluno <cleidson.bononi@gmail.com>
2020-03-10 22:38:12 -0300

    aula

--- BD/biblioteca.sql
+++ BD/biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 11-Mar-2020 às 02:11
+-- Generation Time: 11-Mar-2020 às 02:37
 -- Versão do servidor: 5.7.17
 -- PHP Version: 5.6.30
 
@@ -48,6 +48,44 @@ INSERT INTO `leitor` (`cod`, `nome`, `cpost`, `cidade`) VALUES
 (7, 'Rui', 1200, 'Lisboa'),
 (2, 'Chico', 4000, 'Porto');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livro`
+--
+
+CREATE TABLE `livro` (
+  `nr` smallint(4) NOT NULL,
+  `titulo` varchar(20) NOT NULL,
+  `autor` varchar(20) DEFAULT NULL,
+  `preço` smallint(4) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `livro`
+--
+
+INSERT INTO `livro` (`nr`, `titulo`, `autor`, `preço`) VALUES
+(100, 'Os Maias', 'Eca de Queiroz', 1100),
+(110, 'Os Lusiadas', 'Luis de Camões', 490),
+(120, 'A Selva', 'Ferreira de Castro', 700),
+(130, 'A Capital', 'Eça de Queiroz', 1050),
+(140, 'Terra Fria', 'Ferreira de Castro', 850),
+(150, 'A Reliquia', 'Eça de Queiroz', 900);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `req`
+--
+
+CREATE TABLE `req` (
+  `liv` smallint(4) DEFAULT NULL,
+  `lei` smallint(4) DEFAULT NULL,
+  `datar` date DEFAULT NULL,
+  `datae` date DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -57,6 +95,12 @@ INSERT INTO `leitor` (`cod`, `nome`, `cpost`, `cidade`) VALUES
 --
 ALTER TABLE `leitor`
   ADD PRIMARY KEY (`cod`);
+
+--
+-- Indexes for table `livro`
+--
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`nr`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
